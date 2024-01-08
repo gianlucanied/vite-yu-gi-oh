6bf76f401d539630368478708a403278aa6f81e7
gianlucanied <gianluca.nieddu96@gmail.com>
2024-01-08 15:52:45 +0100

    Add: SingleCard

--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -1,13 +1,17 @@
 <script>
+import SingleCard from './SingleCard.Vue'
 export default{
     name: 'CharactersList',
+    components: {
+        SingleCard
+    }
 }
 </script>
 
 <template>
     <section class="container">
         <div class="col-4">
-            Card della carta
+            <SingleCard/>
         </div>
     </section>
 </template>
