892774123c711a03b40cc06d7b222b317537e180
gianlucanied <gianluca.nieddu96@gmail.com>
2024-01-08 15:32:18 +0100

    Add: AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default{
+    name: 'AppHeader',
+    props: ['message'],
+}
+</script>
+
+<template>
+    <header class="container">
+        <h1>{{ message }}</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+</style>
